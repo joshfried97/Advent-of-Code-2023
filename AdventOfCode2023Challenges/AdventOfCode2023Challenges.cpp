b1b9824565c3ffca346f56543719307e42441681
JFried <joshf@JoshsLaptop>
2024-11-18 11:20:51 +0000

    Main loop set up and ready to go for Day 1 challenge implementation

--- AdventOfCode2023Challenges/AdventOfCode2023Challenges.cpp
+++ AdventOfCode2023Challenges/AdventOfCode2023Challenges.cpp
@@ -1,20 +1,63 @@
-// AdventOfCode2023Challenges.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+// This is my attempt at the Advent of Code 2023 Coding Challenges
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World! \n";
+    std::cout << "Welcome to my attempt at Advent of Code 2023 Challenges! \n";
+
+    // Main loop
+    int choice;
+    do
+    {
+        string promptStr = "Enter challenge to run between 1-24 (enter -1 to exit): \n";
+        std::cout << promptStr;
+        while (!(std::cin >> choice) || (choice > -1 && choice < 1 || choice > 24))
+        {
+            // Enter here if the user entered value incorrect
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+            std::cout << "Invalid input!\n\n" << promptStr;
+        }
+
+        // Switch statement calling the different challenges
+        // No need for a default case as the input handling is done above
+        switch (choice)
+        {
+        case 1:
+            std::cout << "Your choice was " << choice << '\n';
+            break;
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+        case 12:
+        case 13:
+        case 14:
+        case 15:
+        case 16:
+        case 17:
+        case 18:
+        case 19:
+        case 20:
+        case 21:
+        case 22:
+        case 23:
+        case 24:
+            std::cout << "Your choice was " << choice << '\n';
+            break;
+        } 
+
+    } while (choice != -1);
+
+    std::cout << "Thank you for running my challenges, exiting the program now.. \n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
